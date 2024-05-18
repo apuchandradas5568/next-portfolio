b954780612de930cd0eaf82820b81dfec5f5cc25
Apu Chandra Das <apuchandradas5568@gmail.com>
2024-05-18 19:16:34 +0530

    feat: Add favicon to improve user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="app.svg" type="image" />
       <body className={inter.className}>         <ThemeProvider
             attribute="class"
             defaultTheme="dark"
