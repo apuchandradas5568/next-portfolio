c04f78a2e1d9ee7af9bc08ac7a42c283a5d07541
Apu Chandra Das <apuchandradas5568@gmail.com>
2024-05-18 18:22:44 +0530

    done with works

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Approach from "@/components/Approach";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
-import Image from "next/image";
 
 export default function Home() {
   return (
@@ -9,6 +14,12 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
+        <Grid/>
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
       </div>
     </main>
   );
